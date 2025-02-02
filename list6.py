3f8c2bbb4481c79ffb40452d90ae28b086490e22
Nuridin <kajsanuridin@gmail.com>
2025-02-02 16:30:59 +0500

    com

---
+++ list6.py
@@ -0,0 +1,3 @@
+mylist = ['apple', 'banana', 'cherry', 'orange', 'kiwi']
+print(mylist[1:4])
+#output ['banana', 'cherry', 'orange']
---
+++ lits2.py
@@ -0,0 +1,4 @@
+mylist = ['apple', 'banana', 'banana', 'cherry']
+print(mylist[2])
+
+#output banana
